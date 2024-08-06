0ee702b5a235503b7a10234cc03ecc0f685db697
Nancy Usenge <nancyusenge411@gmail.com>
2024-08-06 14:32:43 +0300

    commit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 let booksArray = []
 
 function submit() {
-  const error = document.getElementById("error")
-      error.style.display="none"
+    const error = document.getElementById("error")
+    error.style.display="none"
     const input = document.getElementById("myInput");
     const inputValue = input.value;
     if (inputValue.trim().length===0){
@@ -13,32 +13,32 @@ function submit() {
     const bookContainer = document.getElementById("book-container")
     bookContainer.innerHTML = null
     const loading = document.getElementById("loading")
-      loading.style.display="block"
+    loading.style.display="block"
 
     const apikey="AIzaSyCx71clb4BYk_8sofsdG5SzV1ZFBiGwWTY"
     fetch("https://www.googleapis.com/books/v1/volumes?key=" + apikey + "&q=intitle:" + inputValue.trim())
-  .then(response => response.json())
-  .then(result => {
-    loading.style.display="none"
-    booksArray = result.items
-    booksArray.map((book) => {
-        const individualBook = document.createElement('div')
-        const individualBookImage = document.createElement('img')
-        const individualBookTitle = document.createElement('p')
-        const individualBookAuthor = document.createElement('p')
-        
-        individualBook.className = 'book-div'
-        individualBookImage.className = 'book-image'
-        individualBookImage.src = book.volumeInfo.imageLinks?.smallThumbnail || book.volumeInfo?.previewLink
-        individualBookTitle.innerText = book.volumeInfo.title
-        individualBookAuthor.innerText = book.volumeInfo.authors[0]
-        individualBook.append(individualBookImage)
-        individualBook.append(individualBookTitle)
-        individualBook.append(individualBookAuthor)
+      .then(response => response.json())
+      .then(result => {
+        loading.style.display="none"
+        booksArray = result.items
+        booksArray.map((book) => {
+            const individualBook = document.createElement('div')
+            const individualBookImage = document.createElement('img')
+            const individualBookTitle = document.createElement('p')
+            const individualBookAuthor = document.createElement('p')
+            
+            individualBook.className = 'book-div'
+            individualBookImage.className = 'book-image'
+            individualBookImage.src = book.volumeInfo.imageLinks?.smallThumbnail || book.volumeInfo?.previewLink
+            individualBookTitle.innerText = book.volumeInfo.title
+            individualBookAuthor.innerText = book.volumeInfo.authors[0]
+            individualBook.append(individualBookImage)
+            individualBook.append(individualBookTitle)
+            individualBook.append(individualBookAuthor)
 
-        bookContainer.append(individualBook)
-    })
-  })
+            bookContainer.append(individualBook)
+        })
+     })
 }
 
 
